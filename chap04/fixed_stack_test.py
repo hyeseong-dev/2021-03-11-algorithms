d058e250922f6609dfa73439d9fa22e7292b9165
이혜성 <hyeseong43@gmail.com>
2021-03-24 10:03:50 +0900

    [스택]알고리즘 프로그램 만들기

---
+++ chap04/fixed_stack_test.py
@@ -0,0 +1,56 @@
+# 스택 프로그램 만들기
+# 고정 길이 스택 FixedStack 클래스를 사용한 실습
+
+from enum import Enum
+from fixed_stack import FixedStack  # 모듈로 구분한 ~~.py파일 일때 사용함 현재는 ipynb 파일이기에 사용X
+
+Menu = Enum('Menu', ['푸시', '팝', '피크', '검색', '덤프', '종료'])
+
+def select_menu() -> Menu:
+    '''메뉴 선택'''
+    s = [f'({m.value}){m.name}' for m in Menu]
+    while True:
+        print(*s, sep='   ', end='')
+        n = int(input(': '))
+        if 1 <=n<=len(Menu):
+            return Menu(n)
+
+s = FixedStack(64) # 최대 64개를 push할 수 있는 스택
+
+while True:
+    print(f'현재 데이터 개수: {len(s)} / {s.capacity}')
+    menu = select_menu() # 메뉴 선택
+
+    if menu == Menu.푸시:   # 푸시
+        x = int(input('데이터를 입력하세요. :'))
+        try:
+            s.push(x)
+        except FixedStack.Full:
+            print('스택이 가득 차 있습니다.')
+
+    elif menu == Menu.팝:   # 팝
+        try:
+            s.pop()
+            print(f'팝한 데이터는 {x}입니다.')
+        except FixedStack.Empty:
+          print('스택이 비어 있습니다.')
+
+    elif menu == Menu.피크:   # 피크
+        try:
+            x = s.peek()
+            print(f'peak한 데이터는 {x}입니다.')
+        except FixedStack.Empty:
+            print('스택이 비어 있습니다.')
+
+    elif menu == Menu.검색:   # 검색
+        x = int(input('검색할 값을 입력하세요.'))
+        if x in s:
+            print(f'{s.count(x)}개 포함되고, 맨 앞의 위치는 {s.find(x)}입니다.')
+        else:
+            print('검색 값을 찾을 수 없습니다.')
+
+    elif menu == Menu.덤프:   # 덤프
+        s.dump()
+        
+    else:
+        break # while문 종료
